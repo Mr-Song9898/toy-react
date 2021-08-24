3a620f6e34cb2923eb58c6d854fd899cf28370e7
songwei <mr_song9898@163.com>
2021-08-24 23:58:56 +0800

    feat: add 小游戲

--- main.js
+++ main.js
@@ -1,32 +1,122 @@
 import { createElement, render, Component } from './react';
 
-class MyComponment extends Component {
-  constructor (props) {
-    super();
+// class MyComponment extends Component {
+//   constructor (props) {
+//     super();
 
-    this.state = {
-      a: 1,
-      b: 'b',
-    }
-  }
+//     this.state = {
+//       a: 1,
+//       b: 'b',
+//     }
+//   }
 
+//   render () {
+//     const { a, b } = this.state;
+//     return (
+//      <div>
+//         <div>my component</div>
+//         <button onClick={() => { this.setState({ a: a + 1 }) }}>add</button><br />
+//         <span>{a.toString()}</span>
+//         <span>{b}</span>
+//         {this.children}
+//      </div>
+//     )
+//   }
+// };
+
+// render(<MyComponment id="id" className="c">
+//   <h1>title</h1>
+//   <span>content</span>
+//   <div></div>
+// </MyComponment>, document.body);
+
+
+class Square extends Component {
+  // constructor (props) {
+  //   super(props);
+  // }
   render () {
-    const { a, b } = this.state;
     return (
-     <div>
-        <div>my component</div>
-        <button onClick={() => { this.setState({ a: a + 1 }) }}>add</button><br />
-        <span>{a.toString()}</span>
-        <span>{b}</span>
-        {this.children}
-     </div>
-    )
+      <button className="square" onClick={this.props.onClick}>
+        {this.props.value}
+      </button>
+    );
   }
-};
+}
 
-render(<MyComponment id="id" className="c">
-  <h1>title</h1>
-  <span>content</span>
-  <div></div>
-</MyComponment>, document.body);
+class Board extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null),
+      xIsNext: true,
+    };
+  }
+
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext,
+    });
+  }
+
+  renderSquare(i) {
+    return (
+      <Square
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
+      />
+    );
+  }
+
+  render() {
+    const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+
+    return (
+      <div>
+        <div className="status">{status}</div>
+        <div className="board-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
+      </div>
+    );
+  }
+}
+
+class Game extends Component {
+  render() {
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board />
+        </div>
+        <div className="game-info">
+          <div>{/* status */}</div>
+          <ol>{/* TODO */}</ol>
+        </div>
+      </div>
+    );
+  }
+}
+
+// ========================================
+
+render(
+  <Game />,
+  document.getElementById('root')
+);
 
